aeec106a76ff6a28238b3f0045d5816cc2582778
Alamin Shaikh <hello@octib.com>
2023-11-22 13:23:48 +0600

    Added mergeSortedArrays function

--- src/easy.ts
+++ src/easy.ts
@@ -28,3 +28,32 @@ function findFirstRecurringElement(param: number[]) {
     }
   }
 }
+
+// Merge two sorted arrays
+function mergeSortedArrays(param1: number[], param2: number[]) {
+  const mergedArray: number[] = [];
+
+  let param1Index = 0;
+  let param1Item = param1[param1Index];
+
+  let param2Index = 0;
+  let param2Item = param2[param2Index];
+
+  // Check input
+  if (param1.length === 0) return param2;
+  if (param2.length === 0) return param1;
+
+  while (param1Item || param2Item) {
+    if (!param2Item || param1Item < param2Item) {
+      mergedArray.push(param1Item);
+      param1Item = param1[param1Index + 1];
+      param1Index++;
+    } else {
+      mergedArray.push(param2Item);
+      param2Item = param2[param2Index + 1];
+      param2Index++;
+    }
+  }
+
+  return mergedArray;
+}
