3c0abe95ad52654affd0151817fc7a8f6a4a64ef
Alamin Shaikh <hello@octib.com>
2023-12-20 16:21:35 +0600

    Solved removeDuplicates problem

--- src/easy.ts
+++ src/easy.ts
@@ -161,3 +161,17 @@ function removeElement(nums: number[], val: number): number {
   return count;
 }
 removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
+
+// Problem name - Remove Duplicates from Sorted Array
+// Problem link - https://leetcode.com/problems/remove-duplicates-from-sorted-array
+function removeDuplicates(nums: number[]): number {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+}
+removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
